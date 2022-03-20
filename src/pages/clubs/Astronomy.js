c2c6043672b8b2eadaa024005e8b7591981a5020
Shreyasi2002 <shreyasi18sia@gmail.com>
2022-03-20 20:00:12 +0530

    ClubZen IITK

---
+++ src/pages/clubs/Astronomy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StartHead from '../../components/clubs/astronomy/StartHead'
+import MainPageLayout from '../../components/MainPageLayout'
+
+
+const Astronomy = () => {
+  return (
+      <MainPageLayout color='black'>
+          <StartHead />
+      </MainPageLayout>
+  )
+}
+
+export default Astronomy
